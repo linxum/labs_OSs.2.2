5637a5fc2539f416fd37cbd9e05f8c11e64c0e43
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2023-03-03 09:03:49 +0300

    Update 1.sh

--- 1/1.sh
+++ 1/1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-find $1/ -name *.run -exec sudo chattr +x '{}' \;
+if [ -n "$1" ];
+then
+    find $1/ -name *.run -exec sudo chmod +x '{}' \;
+else
+    echo "Usage: ./1.sh [directory]"
+fi
 exit 0
